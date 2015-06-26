e363190754e56d7b5e8bc85649953acbcd3ebbdd
Laurent Guerin <laguerin@LFR002634.corp.capgemini.com>
2015-06-26 10:25:55 +0200

    Warning removed in JUnit test

--- src/test/java/org/telosys/tools/repository/model/LinkInDbModelTest.java
+++ src/test/java/org/telosys/tools/repository/model/LinkInDbModelTest.java
@@ -2,7 +2,6 @@ package org.telosys.tools.repository.model;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.assertNotNull;
 
 import org.junit.Test;
